345f8588e706e242f917950ee1820e096a614051
shoukatrai <raishoukat033@gmail.com>
2025-07-29 18:08:58 +0500

    changes to order model

--- models/order.js
+++ models/order.js
@@ -1,29 +1,33 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-  customerName: {
-    type: String,
-    required: true,
-  },
-  menuName: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    default: 1,
-  },
-  orderStatus: {
-    type: String,
-    enum: ["pending", "confirmed", "preparing", "delivered", "cancelled"],
-    default: "pending",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  createdBy: String,
-  menuId: String,
+const itemSchema = new mongoose.Schema({
+  menuId: { type: String, required: true },
+  menuName: { type: String, required: true },
+  quantity: { type: Number, default: 1 },
+  price: { type: Number, required: true },
 });
+
+const orderSchema = new mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    items: [itemSchema], 
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["pending", "confirmed", "preparing", "delivered", "cancelled"],
+      default: "pending",
+    },
+    createdBy: { type: String }, 
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const OrderModel = mongoose.model("order", orderSchema);
